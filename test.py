11d41a5d16e5ea129986b2e4f0a26bb363f68795
AnonimMofc <Seluhinvalentin@gmail.com>
2025-06-03 17:25:01 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,110 @@
+from time import sleep
+from random import choice
+#              _______________________________________________ ФУНКЦИИ _____________________________________________________
+def write_text_beatifully(x):
+    for _ in x:
+        print(_, flush=True, end='')
+        sleep(0.04)
+    return ''
+def take_a_card(player):
+    global has_11
+    givepoints = choice(stuck)
+    stuck.remove(givepoints)
+    if givepoints == 11:
+        has_11 = True
+    if givepoints == 11 and player >= 11:
+        print(write_text_beatifully(f'Вы достали карту номинала {givepoints} или 1'))
+        has_11 = False
+        return 1  
+    else:
+        print(write_text_beatifully(f'Вы достали карту номинала {givepoints}'))
+        if has_11 == True and player >= 11:
+            has_11 = False
+            return -10 + givepoints
+        else:
+            return givepoints
+def check_valid(givepoints, player):
+    global has_11
+    if givepoints == 11:
+        has_11 = True
+    if givepoints == 11 and player >= 11:
+        print(write_text_beatifully(f'Вы достали карту номинала {givepoints} или 1'))
+        has_11 = False
+        return 1  
+    else:
+        print(write_text_beatifully(f'Вы достали карту номинала {givepoints}'))
+        if has_11 == True and player+givepoints >= 21:
+            has_11 = False
+            return -10 + givepoints
+        else:
+            return givepoints
+def end_the_game(player, dealer):
+    print(write_text_beatifully(f'У дилера {dealer} очков.'))
+    if dealer < 17:
+        givepoints = choice(stuck)
+        stuck.remove(givepoints)
+        if givepoints == 11 and dealer >= 11:
+            dealer+=1
+        else:
+            dealer+=givepoints
+        print(write_text_beatifully(f'Дилер добрал карту.'), end='\n')
+        return end_the_game(player,dealer)
+    if player > dealer or (player <= 21 and dealer > 21):
+        print(write_text_beatifully('Вы победили.'))
+    elif player < dealer:
+        print(write_text_beatifully('Вы проиграли'))
+    elif player == dealer or (player > 21 and dealer > 21):
+        print(write_text_beatifully('Ничья.'))
+#              ____________________________________________ ВСЕ ВАЖНЫЕ КОЛОДЫ ______________________________________________
+stuck = [2,3,4,5,6,7,8,9,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,11,2,3,4,5,6,7,8,9,10,10,10,11]
+has_11 = False
+#              _____________________________________________________________________________________________________________
+
+print(write_text_beatifully('Происходит раздача карт...'))
+players_points = int(0)
+dealer_points = int(0)
+for y in range(2):
+  tosmbpoints = 0
+  for x in range(2):
+    givepoints = choice(stuck)
+    stuck.remove(givepoints)
+    if givepoints != 11:
+        tosmbpoints += givepoints
+    else:
+        has_11 = True
+        if players_points >= 11:
+            tosmbpoints+=1
+            has_11=False
+        else:
+            tosmbpoints+=givepoints
+    if y == 0:
+        players_points = tosmbpoints
+        if givepoints != 11:
+            print(write_text_beatifully(f'Вы достали карту номинала {givepoints}'))
+        else:
+            print(write_text_beatifully(f'Вы достали карту номинала {givepoints} или 1'))
+    else:
+        dealer_points = tosmbpoints
+while True:
+    if players_points < 22:
+        print(write_text_beatifully(f'У вас {players_points} очков. Желаете взять еще карту?(да/нет)         '))
+        a = input()
+        if a == 'да':
+            players_points+=take_a_card(players_points)
+        elif a == 'нет':
+            end_the_game(players_points, dealer_points)
+            break
+        elif a == 'cheatcode_21numberUNLOCK':
+            print(write_text_beatifully(f'Вы взяли карту номинала {21-players_points}'))
+            print(write_text_beatifully('У вас 21 очков'))
+            end_the_game(21, dealer_points)
+            break
+        elif 'takecard' in a:
+            number = int(a.split(' ')[1])
+            if number:
+                players_points+=check_valid(number, players_points)
+            else:
+                continue
+    else:
+        print(write_text_beatifully(f'У вас {players_points} очков. Вы проиграли.'))
+        break
